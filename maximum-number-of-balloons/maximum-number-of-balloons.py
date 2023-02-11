848ab454be94c04324b4f98c5a3d8b9870bd5c40
nicholasg2001 <118782325+nicholasg2001@users.noreply.github.com>
2023-02-11 11:12:50 -0500

    Time: 37 ms, Memory: 13.9 MB - LeetHub

--- maximum-number-of-balloons/maximum-number-of-balloons.py
+++ maximum-number-of-balloons/maximum-number-of-balloons.py
@@ -1,16 +1,12 @@
 class Solution:
-    def maxNumberOfBalloons(self, s: str) -> int:
-        counts = defaultdict(int)
-        ans = 0
+    def maxNumberOfBalloons(self, text: str) -> int:
+        balloon = Counter('balloon')
+        text_c  = Counter(text)
         
-        for c in s:
-            counts[c]+=1
-            if counts['b']>= 1 and counts['a'] >= 1 and counts['l'] >= 2 and counts['o'] >= 2 and counts['n'] >= 1:
-                ans +=1
-                counts['b']-=1
-                counts['a']-=1
-                counts['l']-=2
-                counts['o']-=2
-                counts['n']-=1
-        return ans
+        min_times = len(text)
+        
+        for c in balloon:
+            min_times = min(min_times, text_c.get(c, 0) // balloon[c])
+        
+        return min_times
         
