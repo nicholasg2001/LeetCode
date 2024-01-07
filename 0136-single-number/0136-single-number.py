4278edf2fd7572ecfbbef3549cc8fa2e050b0b97
Nicholas Gutierrez <118782325+nicholasg2001@users.noreply.github.com>
2024-01-07 16:24:28 -0500

    Time: 107 ms (95.74%), Space: 19.8 MB (23.06%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        
+        ans = 0
+        
+        for num in nums:
+            ans ^= num
+        
+        return ans
+        
