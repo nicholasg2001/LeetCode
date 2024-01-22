7f8bbc5fbb37ec8e1d5847bf8121922c52bbab0b
Nicholas Gutierrez <118782325+nicholasg2001@users.noreply.github.com>
2024-01-22 09:50:53 -0500

    Time: 43 ms (93.33%), Space: 17.4 MB (78.76%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -1,20 +1,27 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
-        l = 0
-        r = len(s)-1
-        vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
-        res = list(s)
+        # Convert the input string to a character array.
+        word = list(s)
+        start = 0
+        end = len(s) - 1
+        vowels = "aeiouAEIOU"
         
-        while(l <= r):
-            if s[l] in vowels and s[r] in vowels:
-                temp = res[l]
-                res[l] = res[r]
-                res[r] = temp
-                l+=1
-                r-=1
-            elif s[l] in vowels and s[r] not in vowels:
-                r-=1
-            else:
-                l+=1
+        # Loop until the start pointer is no longer less than the end pointer.
+        while start < end:
+            # Move the start pointer towards the end until it points to a vowel.
+            while start < end and vowels.find(word[start]) == -1:
+                start += 1
+            
+            # Move the end pointer towards the start until it points to a vowel.
+            while start < end and vowels.find(word[end]) == -1:
+                end -= 1
+            
+            # Swap the vowels found at the start and end positions.
+            word[start], word[end] = word[end], word[start]
+            
+            # Move the pointers towards each other for the next iteration.
+            start += 1
+            end -= 1
         
-        return ''.join(res)
+        # Convert the character array back to a string and return the result.
+        return "".join(word)
