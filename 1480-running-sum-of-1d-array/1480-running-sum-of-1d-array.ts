ffa4e193d2b0a72a84a5d8b00a44ee3668980f10
Nicholas Gutierrez <118782325+nicholasg2001@users.noreply.github.com>
2024-04-25 11:28:04 -0400

    Time: 69 ms (22.22%), Space: 52.3 MB (61.58%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.ts
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.ts
@@ -1,5 +1,11 @@
 function runningSum(nums: number[]): number[] {
-     let result: number = 0;
-     return nums.map(item => result += item);
+    
+    var runningSum: number = 0
+    var sums:number[] = []
+    
+    for(var index in nums){
+        runningSum = runningSum + nums[index];
+        sums.push(runningSum);
+    }
+    return sums;
 };
-
