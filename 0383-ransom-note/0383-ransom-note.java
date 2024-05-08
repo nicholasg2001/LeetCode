774bcabb43659a0d5851b66b7149f25211cf2165
Nicholas Gutierrez <118782325+nicholasg2001@users.noreply.github.com>
2024-05-08 19:11:53 -0400

    Time: 2 ms (81.20%), Space: 44.5 MB (73.07%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,13 +1,15 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        for(char c : magazine.toCharArray()) map.put(c, map.getOrDefault(c, 0) + 1);
+       if (ransomNote.length() > magazine.length()) return false;
+        int[] counter = new int[26];
         
-        for(char c : ransomNote.toCharArray()){
-            if(!map.containsKey(c) || map.get(c) < 1) return false;
-            map.put(c, map.get(c) - 1);
+        for (char c : magazine.toCharArray())
+           counter[c-'a']++;
+
+        for (char c : ransomNote.toCharArray()){
+            if (counter[c-'a'] == 0) return false;
+            counter[c-'a']--;
         }
-        
         return true;
     }
 }
