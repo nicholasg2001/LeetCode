09a4b53e7b0fee94b6d93dc351142a4809422e21
Nicholas Gutierrez <118782325+nicholasg2001@users.noreply.github.com>
2024-05-28 19:55:36 -0400

    Time: 56 ms (5.14%), Space: 44 MB (5.23%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int mySqrt(int x) {
+        
+        int left = 0;
+        int right = x;
+        int mid;
+        int res = 0;
+        
+        while(left <= right){
+            
+            mid = left + Math.floorDiv((right - left), 2);
+            System.out.println(mid);
+            
+            if(Math.pow(mid, 2) > x){
+                right = mid -1;
+            }
+            else if(Math.pow(mid, 2) < x){
+                left = mid + 1;
+                res = mid;
+            } else{
+                return mid;
+            }
+        }
+        return res;
+    }
+}
