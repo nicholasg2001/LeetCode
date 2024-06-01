f750127e128a47a1c0e0997e7514dcba062a7bb8
Nicholas Gutierrez <118782325+nicholasg2001@users.noreply.github.com>
2024-06-01 14:44:12 -0400

    Time: 3 ms (61.86%), Space: 53.1 MB (31.96%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,21 +1,7 @@
 class Solution {
     public int majorityElement(int[] nums) {
         
-        int res = 0;
-        int count = 0;
-        
-        for(int n : nums){
-            
-            if(count == 0){
-                res = n;
-            }
-            
-            if(res == n){
-                count+=1;
-            } else {
-                count-=1;
-            }
-        }
-        return res;
+        Arrays.sort(nums);
+        return nums[Math.floorDiv(nums.length, 2)];
     }
 }
