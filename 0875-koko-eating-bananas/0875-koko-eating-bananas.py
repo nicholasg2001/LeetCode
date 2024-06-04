b9e2c6cb3b05d65a3aca967020b0104e190fc129
Nicholas Gutierrez <118782325+nicholasg2001@users.noreply.github.com>
2024-06-04 12:49:56 -0400

    Time: 269 ms (50.59%), Space: 18.1 MB (87.82%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -15,7 +15,7 @@ class Solution:
                 hours+= math.ceil(p / k)
             
             if hours <= h:
-                res = min(res, k)
+                res = k
                 r = k - 1
             else:
                 l = k + 1
