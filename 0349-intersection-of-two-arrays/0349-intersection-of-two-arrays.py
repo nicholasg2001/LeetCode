5961d7be88546b62d6f288d176c55738f0e93d7d
Nicholas Gutierrez <118782325+nicholasg2001@users.noreply.github.com>
2024-07-09 15:42:56 -0400

    Time: 50 ms (43.31%), Space: 16.6 MB (93.79%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -1,18 +1,16 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         
-        map_ = {}
-        
-        for num in nums1:
-            
-            map_[num] = map_.get(num, 0) + 1
-        
-        
+        seen = set(nums1)
         res = []
+
         for num in nums2:
-            if num in map_:
+            if num in seen:
                 res.append(num)
-                del map_[num]
-            
+                seen.remove(num)
+        
         return res
+    
+
+        
         
