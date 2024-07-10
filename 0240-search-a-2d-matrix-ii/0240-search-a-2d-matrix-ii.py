b7198a599a37d9675812e3fb1dbcbcc695ba0e06
Nicholas Gutierrez <118782325+nicholasg2001@users.noreply.github.com>
2024-07-10 17:58:39 -0400

    Time: 138 ms (61.01%), Space: 22.8 MB (83.18%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
@@ -1,18 +1,18 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         
-        column = 0
-        row = len(matrix) - 1
-        
-        #number at matrix[row][col] smaller than target, then go to next row up
-        #if the number 
-        
-        while(row >= 0 and column < len(matrix[0])):
-            
-            if(matrix[row][column] < target):
-                column+=1
-            
-            elif(matrix[row][column] > target):
+
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        row = rows-1
+        col = 0
+
+        while(row >= 0 and col < cols):
+
+            if(matrix[row][col] < target):
+                col+=1
+            elif(matrix[row][col] > target):
                 row-=1
             else:
                 return True
