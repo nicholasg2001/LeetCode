4822724a0754e8db4840b5219c5d611fbd549338
Nicholas Gutierrez <118782325+nicholasg2001@users.noreply.github.com>
2024-09-04 11:30:23 -0400

    Time: 41 ms (38.23%), Space: 16.6 MB (9.45%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -3,16 +3,19 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+
         n = len(matrix)
-        # Transpose
-        # Swap i, js
+
+
+        #transpose across main diagonal
         for i in range(n):
             for j in range(i+1, n):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
         
+        #reflect horizontaly
+
         for i in range(n):
             for j in range(n // 2):
-                matrix[i][j], matrix[i][n -j - 1] = matrix[i][n-j-1], matrix[i][j]
-
-
+                matrix[i][j], matrix[i][n-j-1] = matrix[i][n-j-1], matrix[i][j]
+        
         
